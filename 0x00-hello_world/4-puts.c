67d331e859d936e305982b0ba10dca4fde3e0f92
LethaboKgatuke1 <kgatukelethabo216@gmail.com>
2023-02-16 08:02:11 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Program printing a line with puts function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multingual puzzle");
+return (0);
+}
