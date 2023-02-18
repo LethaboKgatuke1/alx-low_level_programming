8192e4ed1d1f0bbbcd318c5b83faf9229840c506
LethaboKgatuke1 <kgatukelethabo216@gmail.com>
2023-02-17 17:37:59 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - program printing lowercase alphabet in reverse
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'z' ; i >= 'a' ; i--)
+		putchar('\n');
+	return (0);
+}
+
