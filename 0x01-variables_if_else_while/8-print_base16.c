c11a00fe81ce953da61d9f81e8ad164b9eb04d92
LethaboKgatuke1 <kgatukelethabo216@gmail.com>
2023-02-17 18:24:32 -0800

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,13 +5,15 @@
  *
  * Return: always 0
  */
-input main(void)
+
+int main(void)
 {
 	int i;
+	char j;
 
 	for (i = 0 ; i < 10 ; i++)
 		putchar(i + '0');
-	for (j = 'a' : >= 'f' : j++)
+	for (j = 'a' ; j <= 'f' ; j++)
 		putchar(j);
 	putchar('\n');
 	return (0);
