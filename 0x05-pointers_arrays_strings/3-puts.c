336939c5123e5d917fe751fe8219ffe62e86c7dd
LethaboKgatuke1 <kgatukelethabo216@gmail.com>
2023-02-27 19:03:03 -0800

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * _puts - prints a string
  * @str: The string
@@ -7,7 +8,7 @@
 void _puts(char *str)
 {
 	while (*str)
-		_putchar(*str++);
+		puts(str);
 
 	_putchar('\n');
 }
