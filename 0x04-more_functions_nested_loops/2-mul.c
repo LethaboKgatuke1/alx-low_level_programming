fe7d10249f242a0463de4baaaaa99f4e4733b2d0
LethaboKgatuke1 <kgatukelethabo216@gmail.com>
2023-03-05 09:11:10 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * mul - gives product of two int
+ * @x: int to be multiplied to y
+ * @y: int to be multiplied to x
+ * Return: result of the product
+ */
+
+int mul(int x, int y)
+{
+	int z;
+
+	z = x * y;
+
+	return (z);
+}
