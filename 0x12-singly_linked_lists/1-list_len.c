7ebf88f10bf627f88692b29e461a231b44ae9ee8
LethaboKgatuke1 <kgatukelethabo216@gmail.com>
2023-03-30 08:17:23 -0700

    list length

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "list.h"
+
+/**
+ * list_len - calculates the number of elements
+ * @h: pointer to a list
+ * Return: Integer
+ */
+
+size_t list_len(const list_t *h)
+{
+const list_t *temp;
+unsigned int counter = 0;
+temp = h;
+while (temp)
+{
+counter++;
+temp = temp->next;
+}
+return (counter);
+}
