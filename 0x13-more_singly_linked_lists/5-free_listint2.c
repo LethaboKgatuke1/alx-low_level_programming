b52d614628f731699360d40dff7dd71f96a4a646
LethaboKgatuke1 <kgatukelethabo216@gmail.com>
2023-04-26 01:02:07 -0700

    More Linked List

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists"
+/**
+ * free_listint2 - freeing linked list.
+ * @head: head of a list.
+ *
+ * Return: no return.
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+	listint_t *curr;
+
+	if (head != NULL)
+	{
+		curr = *head;
+		while ((temp = curr) != NULL)
+		{
+			curr = curr->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
